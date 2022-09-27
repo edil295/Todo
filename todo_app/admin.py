841c23b9c7ea236586ae233b82241f422236ef17
edil295 <edil.2905@gmail.com>
2022-09-27 20:49:22 +0600

    added todo admin class

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -3,3 +3,8 @@ from django.contrib import admin
 from .models import Todo
 
 admin.site.register(Todo)
+
+
+@admin.register(Todo)
+class AdminTodo(admin.ModelAdmin):
+    list_display = ('text', 'completed')
