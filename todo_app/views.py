61b25458e53eca3876254897599fc0cccebbca65
edil295 <edil.2905@gmail.com>
2022-09-27 19:49:08 +0600

    first commit

---
+++ todo_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoViewSet(ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
